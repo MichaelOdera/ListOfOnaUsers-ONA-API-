e2a96b6bae19af9c9aa7745775fe77f6ba35262b
MichaelOdera <michogelira@gmail.com>
2020-05-06 17:59:13 +0300

    created a retrofit instance to allow for creating builder pattern classes for integration of the header clases and the base url for the for the Ona Api

---
+++ app/src/main/java/com/example/onaapplication/network/OnaClient.java
@@ -0,0 +1,46 @@
+package com.example.onaapplication.network;
+
+import com.example.onaapplication.models.Constants;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static com.example.onaapplication.models.Constants.ONA_API_KEY;
+
+public class OnaClient {
+    private static Retrofit retrofit = null;
+
+    public static OnaApi getClient(){
+        if(retrofit == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(new Interceptor() {
+                        @NotNull
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request newRequest = chain.request().newBuilder()
+                                    .addHeader("oath_consumer_key", ONA_API_KEY)
+                                    .build();
+                            return chain.proceed(newRequest);
+                        }
+                    })
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(Constants.ONA_BASE_URL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+        }
+        return retrofit.create(OnaApi.class);
+    }
+
+}
